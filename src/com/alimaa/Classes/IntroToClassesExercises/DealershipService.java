e69c5ad0518e1bfb07fb0f0e27df6ca604fa9044
Alimaa <alimaahashim@gmail.com>
2022-02-01 23:04:27 +0000

    Intro to Classes Q6 Attempt

--- src/com/alimaa/Classes/IntroToClassesExercises/DealershipService.java
+++ src/com/alimaa/Classes/IntroToClassesExercises/DealershipService.java
@@ -7,8 +7,13 @@ package com.alimaa.Classes.IntroToClassesExercises;
 //Hint: You will need some way of finding the first empty slot in the array to save the Car in. Maybe checking each slot in the array in turn would help?
 
 public class DealershipService {
-    public DealershipService(CarDealership dealership) { // this connects the 'car dealership class' to the service by running the cardealership class as an argument
+
+    public DealershipService(CarDealership dealership) { // this connects the 'car dealership class' to the service by running the car dealership class as an argument
     }
+
+    public DealershipService(Car addCarToDealership) { // connecting the 'Car' class to the 'Dealership Service' ny running the 'Car' class as an argument
+    }
+
     // counting the number of cars in dealership
 
     public int countingCars(CarDealership dealership) { //NEW METHOD TO COUNT THE NUMBER OF CARS IN THE DEALERSHIP, will return an INT
@@ -18,19 +23,28 @@ public class DealershipService {
 
         for (int i = 0; i < dealership.getCarsInStock().length; i++) { // for i loop
             if (dealership.getCarsInStock()[i] == null) {
-                totalNulls ++; // if the first car in stock within the loop is equal to null
+                totalNulls++; // if the first car in stock within the loop is equal to null
                 // i.e. there is no car in there then the total nulls increases by one each time it goes through the loop.
                 // this will stop at the dealership's max car display number.
             }
         }
-
         int numberOfCars = dealership.getMaxCarDisplayNumber() - totalNulls; // defines the 'number of cars' as the 'max car display number' minus the 'total number of nulls'
 //
         return numberOfCars; // returns the total number of cars within the dealership
 
+    }
 
-//        public String findCarByManufacturer()
+//     WRITE A METHOD TO ADD CARS TO DEALERSHIP
+
+    public void addCarsToDealership(Car addingCars) {
+
+
 
 
     }
+
+
 }
+
+
+//        public String findCarByManufacturer()
