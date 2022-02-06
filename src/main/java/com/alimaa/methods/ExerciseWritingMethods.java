f65cc8545697ecdbcc53a3b8c66028cca1118a9b
Alimaa <alimaahashim@gmail.com>
2022-02-06 14:08:55 +0000

    Methods exercise questions 1 to 5

--- src/main/java/com/alimaa/methods/ExerciseWritingMethods.java
+++ src/main/java/com/alimaa/methods/ExerciseWritingMethods.java
@@ -4,26 +4,93 @@ import java.util.Arrays;
 
 public class ExerciseWritingMethods {
 
-    public static void main(String[] args) {
+    // EXAMPLE - WRITE A METHOD THAT TAKES A NUMBER AS AN ARGUMENT, MULTIPLIES IT BY TWO AND PRINTS THE RESULT TO THE TERMINAL
 
-//Q5 Write a method which takes a string as an argument, capitalises the first letter and returns the capitalised string.
+    // a method that has the data types as an int, that will return a value, should have parameters
+    // multiply that variable by 2 i.e. *2
+    // print the result in the terminal by calling on it in the main
 
-        // Call method here and store result in variable
-        String result =capitalisedFirstLetter("hello");
-        // print variable
-        System.out.println(result);
-
-
+    public static int multiplyingNumberByTwo(int number) {
+        int timesByTwo = number * 2;
+        return timesByTwo;
     }
+
+// Q1 WRITE A METHOD WHICH TAKES TWO NUMBERS AS ARGUMENT AND PRINTS THEIR SUM TO THE TERMINAL.
+    // writing a method that takes numbers as arguments so need to use int and need parameters
+    // write a new variable that will add the two ints within the argument
+    // then return that variable
+    public static int addingTwoNumbersTogether(int number1, int number2){
+        // initialising the two variables as ints
+        int totalSum = number1 + number2;
+        return totalSum;
+    }
+
+// Q2 WRITE A METHOD THAT TAKES A NUMBER AS AN ARGUMENT AND RETURNS THAT NUMBER NEGATED, E.G. PASSING 5 AS AN ARGUMENT SHOULD RETURN -5, PASSING -10 SHOULD RETURN 10.
+    public static int returningNumberNegated(int number){
+        // creating a new method "returningNumberNegated" and passing through an int called number
+        int numberNegated = number *-1;
+        // creating a new variable, using the number variable from above and multiplying it by -1
+//        int numberNegated = number = -number;
+        // another way to do it is number is equal to -number
+         return numberNegated;
+         // returning the 'numberNegated variable'
+    }
+
+// Q3 WRITE A METHOD THAT TAKES TWO NUMBERS AS ARGUMENTS, CHECKS IF THEY ARE THE SAME AND RETURNS THE APPROPRIATE BOOLEAN VALUE.
+//YOU SHOULD CHECK BOTH POSSIBLE OUTCOMES HERE I.E. THAT IT RETURNS TRUE WHEN IT'S SUPPOSED TO AND FALSE WHEN IT'S SUPPOSED TO.
+
+    // create a method that takes 2 ints
+    // create a boolean data type to indicate true and false
+    public static boolean areNumbersAreTheSame (int firstNumber, int secondNumber) {
+        // creating a method with a boolean data type that contains int within the parameters
+        boolean isNotTheSame = false;
+        // instantiating that the variable placeholder 'isNotTheSame' is equal to
+
+        if (firstNumber != secondNumber){
+            return isNotTheSame;
+        } else
+            return true;
+    }
+
+// Q4 WRITE A METHOD THAT TAKES TWO NUMBERS AS ARGUMENTS AND PRINTS ONE OF:
+// 1 - THE FIRST NUMBER IS LARGER
+// 2- THE SECOND NUMBER IS LARGER
+// 3- THE NUMBERS ARE EQUAL
+
+    // a method that takes to number's as arguments
+    public static boolean numbers (int numberOne, int numberTwo) { // boolean that contains int as arguments
+        boolean firstNumberIsLarger = numberOne > numberTwo; // need a boolean data type, so it can be used within the if else condition
+//        boolean secondNumberIsLarger = numberOne < numberTwo;
+        boolean theNumbersAreEqual = numberOne == numberTwo ;
+
+        // if else statement so that if the values for variable numberOne is more than  numberTwo it will return true. if numberTwo is bigger then it returns false.
+        if (numberOne > numberTwo) {
+            return firstNumberIsLarger;
+        } else if (numberOne < numberTwo) {
+            return false;
+        } else {
+            // this one isn't functioning as expected
+            return theNumbersAreEqual;
+        }
+    }
+
+//Q5 WRITE A METHOD WHICH TAKES A STRING AS AN ARGUMENT, CAPITALISES THE FIRST LETTER AND RETURNS THE CAPITALISED STRING.
+
+    // I CHANGED IT SO IT CAN BE PRINTED IN THE MAIN
+
+    // Call method here and store result in variable
+//        String result =capitalisedFirstLetter("hello");
+    // print variable
+//        System.out.println(result);
 
     // write method here
     // Pseudocode - i.e. plain English description of what code will do
 
     // Method signature - take a string as an argument then return a string
-    public static String capitalisedFirstLetter(String inputString){
+    public static String capitalisedFirstLetter(String inputString) {
 
         // Capitalise first letter of inputString
-        String capitalisedString = inputString.substring(0,1).toUpperCase() + inputString.substring(1);
+        String capitalisedString = inputString.substring(0, 1).toUpperCase() + inputString.substring(1);
         // .subString is taking a small section of the String in this case its starting at index 0 and stopping at index 1
         // then it's going to capitalise that one index
         // then adds (+) it to the rest of the subString
@@ -34,9 +101,10 @@ public class ExerciseWritingMethods {
     }
 
     //
-    public static void convertArrayToString(String[] strings){
+    public static void convertArrayToString(String[] strings) {
         String v1 = Arrays.toString(strings);
-// this .toString s a methodTOsTRING IS A METHOD USED WITHIN ARRAY CALSS , THE METHOD NEEDS AN ARRAY TO BE PASSED IN IT
+// this .toString is a method
+// . toString IS A METHOD USED WITHIN ARRAY CLASS, THE METHOD NEEDS AN ARRAY TO BE PASSED IN IT
         System.out.println("v1 " + v1);
 
         String v2 = strings.toString();
@@ -45,5 +113,11 @@ public class ExerciseWritingMethods {
 
 
     }
+
+// Q6 WRITE A METHOD THAT TAKES AN ARRAY OF STRINGS AS AN ARGUMENT AND RETURNS THE NUMBER OF ITEMS IN THE ARRAY
+
+
+// Q7 WRITE A METHOD THAT TAKES A SENTENCE AS AN ARGUMENT, COUNTS THE NUMBER OF WORDS IN THE ARRAY AND PRINTS " THE SENTENCE CONTAINS x WORDS"
+// USE WHAT YOU ALREADY KNOW, MAYBE REUSE ONE OF THE METHODS ABOVE.
 
 }
---
+++ src/main/java/com/alimaa/methods/Main.java
@@ -0,0 +1,56 @@
+package com.alimaa.methods;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+// EXAMPLE - WRITE A METHOD THAT TAKES A NUMBER AS AN ARGUMENT, MULTIPLIES IT BY TWO AND PRINTS THE RESULT TO THE TERMINAL
+
+        System.out.println( 6 + " multiplied by two is = " + ExerciseWritingMethods.multiplyingNumberByTwo(6));
+        // calling on the class where the method can be found then . name of method we want to print from
+        System.out.println("_______________");
+
+ // Q1 WRITE A METHOD WHICH TAKES TWO NUMBERS AS ARGUMENT AND PRINTS THEIR SUM TO THE TERMINAL.
+
+        System.out.println(ExerciseWritingMethods.addingTwoNumbersTogether(4, 6)); // calling on the calls and then the method we want to print from
+        System.out.println("_______________");
+
+// Q2 WRITE A METHOD THAT TAKES A NUMBER AS AN ARGUMENT AND RETURNS THAT NUMBER NEGATED, E.G. PASSING 5 AS AN ARGUMENT SHOULD RETURN -5, PASSING -10 SHOULD RETURN 10.
+        System.out.println(ExerciseWritingMethods.returningNumberNegated(4));
+        System.out.println("_______________");
+
+
+// Q3 WRITE A METHOD THAT TAKES TWO NUMBERS AS ARGUMENTS, CHECKS IF THEY ARE THE SAME AND RETURNS THE APPROPRIATE BOOLEAN VALUE.
+        System.out.println(ExerciseWritingMethods.areNumbersAreTheSame(11,7));
+        System.out.println("_______________");
+
+// Q4 WRITE A METHOD THAT TAKES TWO NUMBERS AS ARGUMENTS AND PRINTS ONE OF:
+// 1 - THE FIRST NUMBER IS LARGER
+// 2- THE SECOND NUMBER IS LARGER
+// 3- THE NUMBERS ARE EQUAL
+
+        System.out.println("The first number is larger = " + ExerciseWritingMethods.numbers(4,7)); // works
+        System.out.println("_______________");
+
+        System.out.println("The second number is larger = " + ExerciseWritingMethods.numbers(9,7)); // works 
+        System.out.println("_______________");
+
+        System.out.println("The numbers are equal = " + ExerciseWritingMethods.numbers(5,5));
+        // this is printing false if the second number is over 5 but prints true it's less than five ???
+        System.out.println("_______________");
+
+
+//Q5 WRITE A METHOD WHICH TAKES A STRING AS AN ARGUMENT, CAPITALISES THE FIRST LETTER AND RETURNS THE CAPITALISED STRING.
+        // Call method here and store result in variable and printing the variable
+        System.out.println(ExerciseWritingMethods.capitalisedFirstLetter("hello"));
+        System.out.println("_______________");
+
+    }
+
+// Q6 WRITE A METHOD THAT TAKES AN ARRAY OF STRINGS AS AN ARGUMENT AND RETURNS THE NUMBER OF ITEMS IN THE ARRAY
+
+
+// Q7 WRITE A METHOD THAT TAKES A SENTENCE AS AN ARGUMENT, COUNTS THE NUMBER OF WORDS IN THE ARRAY AND PRINTS " THE SENTENCE CONTAINS x WORDS"
+// USE WHAT YOU ALREADY KNOW, MAYBE REUSE ONE OF THE METHODS ABOVE.
+
+}
